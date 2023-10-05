879b535ff7fa7f3c97c695e42f14517ff72c14f0
oalmhage <142837730+oalmhage@users.noreply.github.com>
2023-10-05 13:54:05 +0200

    Add project files.

---
+++ Inlämningsuppgift4/Program.cs
@@ -0,0 +1,192 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace Vaccination
+{
+    public class Person
+    {
+        public string Name;
+        public string Age; 
+        public bool WorksInHealthCare;
+        public bool RiskGroup;
+        public bool HasBeenInfected;
+        public int RequiredDoses;
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
+            Console.WriteLine("Huvudmeny: ");
+            Console.WriteLine();
+            Console.WriteLine("Antal tillgängliga doser: ");
+            Console.WriteLine("Vaccinering under 18 år: ");
+            Console.WriteLine("Indatafil: ");
+            Console.WriteLine("Utdatafil: ");
+
+            bool running = true;
+            while (running)
+            {
+                int option = ShowMenu("Vad vill du göra? ", new[]
+                {
+
+                 "Skapa prioritetsordning",
+                 "Ändra antal vaccindoser",
+                 "Ändra åldersgräns",
+                 "Ändra indatafil",
+                 "Ändra utdatafill",
+                 "Avsluta",
+
+               });
+
+                Console.Clear();
+
+                if (option == 0)
+                {
+                    //PriorityOrder();
+                }
+                else if (option == 1)
+                {
+                    //VaccinQuantity();
+                }
+                else if (option == 2)
+                {
+                    //AgeLimit();
+                }
+                else if (option == 3)
+                {
+                    //InputFile();
+                }
+                else if (option == 4)
+                {
+                    //OutputFile();
+                }
+                else
+                {
+                    running = false;
+                    Console.WriteLine("Hejdå!");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        
+        // Create the lines that should be saved to a CSV file after creating the vaccination order.
+        //
+        // Parameters:
+        //
+        // input: the lines from a CSV file containing population information
+        // doses: the number of vaccine doses available
+        // vaccinateChildren: whether to vaccinate people younger than 18
+        public static string[] CreateVaccinationOrder(string[] input, int doses, bool vaccinateChildren)
+        {
+            // Replace with your own code.
+            return new string[0];
+        }
+
+        public static int ShowMenu(string prompt, IEnumerable<string> options)
+        {
+            if (options == null || options.Count() == 0)
+            {
+                throw new ArgumentException("Cannot show a menu for an empty list of options.");
+            }
+
+            Console.WriteLine(prompt);
+
+            // Hide the cursor that will blink after calling ReadKey.
+            Console.CursorVisible = false;
+
+            // Calculate the width of the widest option so we can make them all the same width later.
+            int width = options.Max(option => option.Length);
+
+            int selected = 0;
+            int top = Console.CursorTop;
+            for (int i = 0; i < options.Count(); i++)
+            {
+                // Start by highlighting the first option.
+                if (i == 0)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                var option = options.ElementAt(i);
+                // Pad every option to make them the same width, so the highlight is equally wide everywhere.
+                Console.WriteLine("- " + option.PadRight(width));
+
+                Console.ResetColor();
+            }
+            Console.CursorLeft = 0;
+            Console.CursorTop = top - 1;
+
+            ConsoleKey? key = null;
+            while (key != ConsoleKey.Enter)
+            {
+                key = Console.ReadKey(intercept: true).Key;
+
+                // First restore the previously selected option so it's not highlighted anymore.
+                Console.CursorTop = top + selected;
+                string oldOption = options.ElementAt(selected);
+                Console.Write("- " + oldOption.PadRight(width));
+                Console.CursorLeft = 0;
+                Console.ResetColor();
+
+                // Then find the new selected option.
+                if (key == ConsoleKey.DownArrow)
+                {
+                    selected = Math.Min(selected + 1, options.Count() - 1);
+                }
+                else if (key == ConsoleKey.UpArrow)
+                {
+                    selected = Math.Max(selected - 1, 0);
+                }
+
+                // Finally highlight the new selected option.
+                Console.CursorTop = top + selected;
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.ForegroundColor = ConsoleColor.White;
+                string newOption = options.ElementAt(selected);
+                Console.Write("- " + newOption.PadRight(width));
+                Console.CursorLeft = 0;
+                // Place the cursor one step above the new selected option so that we can scroll and also see the option above.
+                Console.CursorTop = top + selected - 1;
+                Console.ResetColor();
+            }
+
+            // Afterwards, place the cursor below the menu so we can see whatever comes next.
+            Console.CursorTop = top + options.Count();
+
+            // Show the cursor again and return the selected option.
+            Console.CursorVisible = true;
+            return selected;
+        }
+    }
+
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            // Arrange
+            string[] input =
+            {
+                "19720906-1111,Elba,Idris,0,0,1",
+                "8102032222,Efternamnsson,Eva,1,1,0"
+            };
+            int doses = 10;
+            bool vaccinateChildren = false;
+
+            // Act
+            string[] output = Program.CreateVaccinationOrder(input, doses, vaccinateChildren);
+
+            // Assert
+            Assert.AreEqual(output.Length, 2);
+            Assert.AreEqual("19810203-2222,Efternamnsson,Eva,2", output[0]);
+            Assert.AreEqual("19720906-1111,Elba,Idris,1", output[1]);
+        }
+    }
+}
